511d1483ace87fe3f7eadba0d6b9bf74e6a34b26
Christophe Meyer <christophe.meyer.pro@gmail.com>
2017-12-01 20:17:22 -0500

    nbconvert relative path

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 # Times out with the default limit of 30 seconds (package load time, precompilation, etc.).
 const JUPYTER_BIN = readstring(open(joinpath(Pkg.dir("IJulia"), "deps", "JUPYTER")))
-const CMD = `$JUPYTER_BIN nbconvert --ExecutePreprocessor.timeout=600 --to html --output-dir=. --stdout --execute src/DistributionsPlot.jl.ipynb`
+const CMD = `$JUPYTER_BIN nbconvert --ExecutePreprocessor.timeout=600 --to html --output-dir=. --stdout --execute ../src/DistributionsPlot.jl.ipynb`
 run(CMD)
 
